e0f102dac673a42f03bbfa1fcdcaf6fa5a21db7a
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-12-30 21:38:50 +0700

    feat : add route dashboard

--- app/routes.ts
+++ app/routes.ts
@@ -10,7 +10,8 @@ export default [
     index("routes/home.tsx"),
     route("signin", "routes/signin.tsx"), //post /auth/signin
     route("signup", "./routes/signup.tsx"), // POST /auth/signup
-    route("dashboard", "routes/dashboard.tsx"), //get/auth/me
+    route("dashboard", "routes/dashboard.tsx"), //get/auth/mE
+
     route("signout", "./routes/signout.tsx"),
     route("bookingformscreen", "./routes/bookingformscreen.tsx"),
   ]),
