fa22139ac7296243b2bb30d08f94e1573aba39b3
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-12-30 21:53:26 +0700

    feat : add routes booking-form & result-search

--- app/routes.ts
+++ app/routes.ts
@@ -10,8 +10,9 @@ export default [
     index("routes/home.tsx"),
     route("signin", "routes/signin.tsx"), //post /auth/signin
     route("signup", "./routes/signup.tsx"), // POST /auth/signup
-    route("dashboard", "routes/dashboard.tsx"), //get/auth/mE
-
+    route("dashboard", "routes/dashboard.tsx"), //get/auth/me
+    route("bookingform", "./routes/booking-form.tsx"),
+    route("resultsearch", "./routes/result-search.tsx"),
     route("signout", "./routes/signout.tsx"),
     route("bookingformscreen", "./routes/bookingformscreen.tsx"),
   ]),
