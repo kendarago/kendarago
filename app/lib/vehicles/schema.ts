1d6ee0d5473e190911c1c2cde02b6eafc2af427a
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2026-02-03 23:05:05 +0700

    feat: add rental company schema to vehicle schema

--- app/lib/vehicles/schema.ts
+++ app/lib/vehicles/schema.ts
@@ -1,4 +1,50 @@
+// import { z } from "zod";
+
+// export const VehicleSchema = z.object({
+//   id: z.string(),
+//   slug: z.string(),
+//   rentalCompanySlug: z.string(),
+//   engineCapacity: z.string(),
+//   imageUrl: z.string(),
+//   brand: z.string(),
+//   name: z.string(),
+//   seatCapacity: z.number(),
+//   pricePerDay: z.number(),
+//   year: z.number(),
+//   vehicleTypeSlug: z.enum(["motorcycle", "car"]),
+//   transmission: z.enum(["Matic", "Manual"]),
+//   fuelType: z.enum(["Gas", "Electric"]),
+
+//   rentalCompany: RentalCompanySchema,
+// });
+
+// export const RentalCompanySchema = z.object({
+//   id: z.string(),
+//   slug: z.string(),
+//   name: z.string(),
+//   address: z.string(),
+//   city: z.string(),
+//   operatingHours: z.string(),
+//   contact: z.string(),
+//   createdAt: z.string(),
+//   updatedAt: z.string(),
+// });
+
+// export type Vehicle = z.infer<typeof VehicleSchema>;
+
 import { z } from "zod";
+
+export const RentalCompanySchema = z.object({
+  id: z.string(),
+  slug: z.string(),
+  name: z.string(),
+  address: z.string(),
+  city: z.string(),
+  operatingHours: z.string(),
+  contact: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+});
 
 export const VehicleSchema = z.object({
   id: z.string(),
@@ -14,6 +60,7 @@ export const VehicleSchema = z.object({
   vehicleTypeSlug: z.enum(["motorcycle", "car"]),
   transmission: z.enum(["Matic", "Manual"]),
   fuelType: z.enum(["Gas", "Electric"]),
+  rentalCompany: RentalCompanySchema,
 });
 
 export type Vehicle = z.infer<typeof VehicleSchema>;
